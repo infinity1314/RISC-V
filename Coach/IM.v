42cbc3c20eb78a75c178e4eb95943015847faf73
GWwzs <GWwzs20051015@outlook.com>
2026-04-30 10:51:53 +0800

    IM

--- Coach/IM.v
+++ Coach/IM.v
@@ -4,13 +4,14 @@
 module IM(InsMemRW, addr, Ins);
     input         InsMemRW; // 指令存储器读使能信号
     input  [11:2] addr;     // 指令地址（字对齐）
+    input  clk;
     output reg [31:0] Ins;  // 输出的指令码
 
     // 定义 1024 个 32 位指令存储单元
     reg [31:0] memory[0:1023];
 
     // 组合逻辑读取（根据地址实时输出指令）
-    always @(addr or InsMemRW) begin
+    always @(posedge clk) begin
         if (InsMemRW) begin
             Ins <= memory[addr]; // 读取指令
         end
